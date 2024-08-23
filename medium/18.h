6646225f96cfeedd878a7494b37cc62d610bf6cb
Vito258 <3090095224@qq.com>
2024-08-23 15:00:52 +0800

    11111

---
+++ medium/18.h
@@ -0,0 +1,21 @@
+//
+// Created by 860122023 on 2024/8/21.
+//
+
+#ifndef ALGORITHM_TEST_18_H
+#define ALGORITHM_TEST_18_H
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+/* 四数之和 */
+class Solution {
+public:
+    std::vector<std::vector<int>> fourSum(std::vector<int>& nums, int target) {
+        // 先进行排序
+        std::sort(nums.begin(),nums.end());
+
+        // 锚定两个值
+    }
+};
+#endif //ALGORITHM_TEST_18_H
